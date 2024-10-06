76bd90de5c1935bdf51dca65cfb977901d714b39
bitcat <kurohaj96@gmail.com>
2024-10-07 06:57:45 +0900

    change: Footerにソースコードのリンクを追加

--- components/common/Footer.tsx
+++ components/common/Footer.tsx
@@ -2,6 +2,9 @@ const Footer = () => {
   return (
     <footer className="bg-zinc-800 text-white p-4 text-center">
       <p>© {new Date().getFullYear()} Joe Kuroha</p>
+      <a href="https://github.com/joesandao/joe-portfolio" className="text-blue-400 hover:underline">
+        This site is powered by Next.JS 14
+      </a>
     </footer>
   );
 };

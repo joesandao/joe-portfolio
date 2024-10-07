08046aef8fc708eb3acfe585788a3d8f030844fb
bitcat <kurohaj96@gmail.com>
2024-10-08 03:32:52 +0900

    fix: 無駄なインポートを削除

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
-import '../components/common/Footer'
 
 const inter = Inter({ subsets: ['latin'] })
 

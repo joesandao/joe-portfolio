ac72bf34535efbc4fc288b245073f7cbb6fb1f9b
bitcat <kurohaj96@gmail.com>
2024-10-08 03:50:15 +0900

    ライトモードだと文字が全て消えてしまう問題を修正

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
   return (
     <html lang="ja">
       <body className={inter.className}>
-        <div className='bg-black'>
+        <div className='bg-black text-white'>
          {children}
         </div>
       </body>

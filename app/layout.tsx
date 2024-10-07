717f92481db93f448c99dba8817b1d866bfcdd2b
bitcat <kurohaj96@gmail.com>
2024-10-08 03:29:30 +0900

    fix: 使ってないライブラリのインポートを削除

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
 import '../components/common/Footer'
-import Footer from '../components/common/Footer'
 
 const inter = Inter({ subsets: ['latin'] })
 
